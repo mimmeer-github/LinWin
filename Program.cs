986ae7afef6c1a28af845438406e96f564e249fe
Mimmeer <archietablet99@gmail.com>
2024-12-12 07:19:33 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using LinWin.terminal_apps.ls;
+using LinWin.terminal_apps.help;
+
+namespace LinWinTerminal
+{
+    class LinWin
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string command;
+
+                if (args.Length > 0)
+                {
+                    command = args[0];
+                    args = args.Skip(1).ToArray(); // Remove the first argument
+                }
+                else
+                {
+                    Console.Write("> ");
+                    command = Console.ReadLine();
+                }
+
+                string[] parts = command.Split(' ');
+                string commandName = parts[0];
+                string[] commandArgs = parts.Skip(1).ToArray();
+
+                if (commandName == "exit")
+                {
+                    break;
+                }
+                else if (commandName == "ls")
+                {
+                    Ls.LsCommand(commandArgs);
+                }
+                else if (commandName == "help")
+                {
+                    Help.HelpCommand();
+                }
+                else if (commandName == "argtest")
+                {
+                    foreach (string arg in commandArgs)
+                    {
+                        Console.WriteLine(arg);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Unknown command.");
+                }
+            }
+        }
+    }
+}
---
+++ terminal-apps/man/Man.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinWin.terminal_apps.man
+{
+    internal class Man
+    {
+        public static void ManCommand() 
+        {
+
+        }
+    }
+}
